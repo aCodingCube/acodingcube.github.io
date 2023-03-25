a91944a17703cac8113174327d191a9e821f9f63
aGeniusCube <128707689+aCodingCube@users.noreply.github.com>
2023-03-25 17:01:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("mousedown",MouseDown);
-window.addEventListener("mouseup",MouseUp);
-function MouseDown(){
-    document.getElementById("Feedback").innerHTML = "Mousebutton pressed!"
-}
-function MouseUp(){
-    document.getElementById("Feedback").innerHTML = "Mousebutton released!"
-}
